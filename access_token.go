add5eb595ef4093fb4338a57ffed89a52f7eddad
Parag Patel <parag.patel.one@gmail.com>
2014-02-21 09:57:01 -0500

    Update access_token.go

--- access_token.go
+++ access_token.go
@@ -1,21 +1,6 @@
 package oauth1
 
 /*
-@author: Dana McCallum
-@author: Parag Patel
-
-
-It uses the escape and isEscapable function that are taken from https://github.com/mrjones/oauth/blob/master/oauth.go
-
-This code adds a authentication header to your requests.   It will not pull tokens from the server.   You need at a minimum a consumer key and
-consumer address.
-
-TODO: The unit tests at access_token_test.go need to be redone, though they are all passing currently
-
-Go Verison Supported: 1.2
-
-Here is some example code that might you might use. You must encode the url parameters before creating a request.  I use a custom functions that
-encodes them EncodeURLParameters.  Here we just use 1 function and 1 method from oauth1, NewAccessToken, and SignRequestHeader.   That is all you need.
 
 
 	token := oauth1.NewAccessToken("consumer_Key", "consumer_secret", "", "")
